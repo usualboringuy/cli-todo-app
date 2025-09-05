5b2300dd1675473855bf9d334d7f41a3330844b9
Anish Pechetti <work.anish.pechetti@gmail.com>
2025-09-05 09:48:39 +0000

    Made Functions and Methods for managing the Application

---
+++ pkg/todo/todo.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Todo struct {
+	Title       string
+	Completed   bool
+	CreatedOn   time.Time
+	CompletedOn *time.Time
+}
+
+type Todos []Todo
+
+func (todos *Todos) add(title string) {
+	todo := Todo{
+		Title:       title,
+		Completed:   false,
+		CompletedOn: nil,
+		CreatedOn:   time.Now(),
+	}
+	*todos = append(*todos, todo)
+}
+
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+		err := errors.New("invalid index")
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (todos *Todos) delete(index int) error {
+	t := *todos
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+	*todos = append(t[:index], t[index+1:]...)
+	return nil
+}
